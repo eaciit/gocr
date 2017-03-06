ed6ecad42268259f9601d8314685ccbf5cbf1699
Bagus Cahyono <baguscah77@gmail.com>
2017-03-06 14:40:11 +0700

    Adding binarization image to 2D array using external lib

--- train/train.go
+++ train/train.go
@@ -6,10 +6,12 @@ import (
 	_ "image/jpeg"
 	_ "image/png"
 	"os"
+
+	"github.com/anthonynsimon/bild/segment"
 )
 
-// Read image in path to 2D array with size based on image width and height
-func ReadImageWithPath(path string) [][]uint8 {
+// Read image in path
+func ReadImage(path string) image.Image {
 	// Read the file
 	infile, err := os.Open(path)
 	if err != nil {
@@ -25,6 +27,11 @@ func ReadImageWithPath(path string) [][]uint8 {
 		panic(err)
 	}
 
+	return src
+}
+
+//Convert image to grayscale 2D array
+func ImageToGraysclaeArray(src image.Image) [][]uint8 {
 	// Convert the image to Grayscale
 	bounds := src.Bounds()
 	w, h := bounds.Max.X, bounds.Max.Y
@@ -37,10 +44,12 @@ func ReadImageWithPath(path string) [][]uint8 {
 		}
 	}
 
-	// Initialize 2D array for the gray value of the image
-	imageArr := make([][]uint8, gray.Bounds().Max.X, gray.Bounds().Max.Y)
+	// Initialize 2D array for the gray value of the image (row first)
+	imageArr := make([][]uint8, gray.Bounds().Max.X)
 
 	for x := 0; x < gray.Bounds().Max.X; x++ {
+		// Intialize the column
+		imageArr[x] = make([]uint8, gray.Bounds().Max.Y)
 		for y := 0; y < gray.Bounds().Max.Y; y++ {
 			imageArr[x][y] = gray.GrayAt(x, y).Y
 		}
@@ -48,4 +57,31 @@ func ReadImageWithPath(path string) [][]uint8 {
 
 	// Return the 2D array
 	return imageArr
+}
+
+func ImageToBinaryArray(src image.Image) [][]uint8 {
+	// FIXME: still finding the best Threshold
+	gray := segment.Threshold(src, 128)
+
+	// Initialize 2D array for the gray value of the image (row first)
+	imageArr := make([][]uint8, gray.Bounds().Max.X)
+
+	for x := 0; x < gray.Bounds().Max.X; x++ {
+		// Intialize the column
+		imageArr[x] = make([]uint8, gray.Bounds().Max.Y)
+		for y := 0; y < gray.Bounds().Max.Y; y++ {
+			imageArr[x][y] = gray.GrayAt(x, y).Y / 255
+		}
+	}
+
+	return imageArr
+}
+
+// Binarize the given imageArr using
+// Best algorithm based on this paper https://pdfs.semanticscholar.org/6347/5461213fdaa24e418c33454c72bdbbe8f8b4.pdf is Sauvola
+// Sauvola Reference: http://www.mediateam.oulu.fi/publications/pdf/24.p
+// TODO: Implement Sauvola algorithm
+func SauvolaBinarization(imageArr [][]uint8) [][]uint8 {
+
+	return nil
 }
