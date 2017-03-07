c12e1897a0abfddc9a4b148f6ec1ceecb1e94a49
Bagus Cahyono <baguscah77@gmail.com>
2017-03-07 17:55:58 +0700

    Updating example using LinearScan

--- example/main.go
+++ example/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/eaciit/gocr"
 )
@@ -21,17 +21,45 @@ var (
 	}() + "/../model/"
 )
 
+type Marker struct {
+	Start int
+	End   int
+}
+
+func (m Marker) thickness() int {
+	return m.End - m.Start
+}
+
 func main() {
 	// image := gocr.ReadImage(samplePath + "sample1/0.gif")
 	// binaryArr := gocr.ImageToBinaryArray(image)
 
 	// Downloading english font dataset and index.csv
 	d, _ := os.Getwd()
-	gocr.Prepare(d + "/")
+	// gocr.Prepare(d + "/")
+	//
+	// // Load the sample data and save it in .gob file
+	// gocr.Train(samplePath+"sample1/", modelPath+"sample1/")
+	// // Load the model data and return it
+	// model := gocr.ReadModel(modelPath + "sample1/model.gob")
+	// fmt.Println(len(model.ModelImages))
 
-	// Load the sample data and save it in .gob file
-	gocr.Train(samplePath+"sample1/", modelPath+"sample1/")
-	// Load the model data and return it
-	model := gocr.ReadModel(modelPath + "sample1/model.gob")
-	fmt.Println(len(model.ModelImages))
+	image := gocr.ReadImage(d + "/imagetext_3.png")
+	iA := gocr.ImageToBinaryArray(image)
+	gocr.ImageArrayToImage(iA, d+"/result/result.png")
+	data := gocr.ImageArrayToMatrix(iA)
+	gocr.MatrixToImage(data, d+"/result/result2.png")
+
+	lines, charss := gocr.LinearScan(data)
+
+	for i, line := range lines {
+		gocr.MatrixToImage(line, d+"/result/line_"+strconv.Itoa(i)+".png")
+	}
+
+	for i, chars := range charss {
+		for j, char := range chars {
+			gocr.MatrixToImage(char, d+"/result/char_"+strconv.Itoa(i)+strconv.Itoa(j)+".png")
+		}
+	}
+
 }
