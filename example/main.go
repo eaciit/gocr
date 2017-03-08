8f0ba212f2e1a9fe7e739ea6bf55e06b0ef21e06
Bagus Cahyono <baguscah77@gmail.com>
2017-03-08 17:08:23 +0700

    Updating example

--- example/main.go
+++ example/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"fmt"
 	"os"
-	"strconv"
 
 	"github.com/eaciit/gocr"
 )
@@ -26,18 +26,22 @@ func main() {
 	d, _ := os.Getwd()
 	// gocr.Prepare(d + "/")
 	//
-	// // Load the sample data and save it in .gob file
-	// gocr.Train(samplePath+"sample1/", modelPath+"sample1/")
-	//
-	// // Load the model data and return it
-	// model, err := gocr.ReadModel(modelPath + "sample1/model.gob")
+	// Load the sample data and save it in .gob file
+	// err := gocr.Train(samplePath+"/Sample2/", modelPath+"/Sample2/")
 	// if err != nil {
 	// 	panic(err)
 	// }
-	// fmt.Println(len(model.ModelImages))
+
+	// Load the model data and return it
+	model, err := gocr.ReadModel(modelPath + "/Sample2/model.gob")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("ModelImage count: ", len(model.ModelImages))
 
 	// Read the image
-	image, err := gocr.ReadImage(d + "/imagetext_3.png")
+	image, err := gocr.ReadImage(d + "/imagetext_1.png")
 	if err != nil {
 		panic(err)
 	}
@@ -50,18 +54,26 @@ func main() {
 	gocr.MatrixToImage(data, d+"/result/result2.png")
 
 	// Scan and slice
-	lines, charss := gocr.LinearScan(data)
+	_, charss := gocr.LinearScan(data)
 
 	// Save each line to image
-	for i, line := range lines {
-		gocr.MatrixToImage(line, d+"/result/line_"+strconv.Itoa(i)+".png")
-	}
+	// for i, line := range lines {
+	// 	gocr.MatrixToImage(line, d+"/result/line_"+strconv.Itoa(i)+".png")
+	// }
 
 	// Save each char to image
-	for i, chars := range charss {
-		for j, char := range chars {
-			gocr.MatrixToImage(char, d+"/result/char_"+strconv.Itoa(i)+strconv.Itoa(j)+".png")
+	// for i, chars := range charss {
+	// 	for j, char := range chars {
+	// 		gocr.MatrixToImage(char, d+"/result/char_"+strconv.Itoa(i)+strconv.Itoa(j)+".png")
+	// 	}
+	// }
+
+	for _, chars := range charss {
+		for _, char := range chars {
+			fmt.Print(gocr.Predict(char, &model))
 		}
+
+		fmt.Println("")
 	}
 
 }
