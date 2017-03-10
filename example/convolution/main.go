9d9d1dd845a5c55e3b931565fb5c3e1591eedff4
Bagus Cahyono <baguscah77@gmail.com>
2017-03-10 13:35:32 +0700

    Adding example for using convolution function

---
+++ example/convolution/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/eaciit/gocr"
+	"github.com/gonum/matrix/mat64"
+)
+
+func main() {
+
+	k1 := mat64.NewDense(2, 2, []float64{1, 2, 3, 4})
+	k2 := mat64.NewDense(2, 2, []float64{5, 6, 7, 8})
+
+	kernel := [][]*mat64.Dense{{k1, k2}, {k1, k2}}
+
+	d1 := mat64.NewDense(4, 4, []float64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16})
+	d2 := mat64.NewDense(4, 4, []float64{17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32})
+
+	data := [][]*mat64.Dense{{d1, d2}}
+
+	r := gocr.Convolve(data, kernel, 0, 2)
+
+	fmt.Println("Result -")
+	printMatrix(r[0][0])
+	printMatrix(r[0][1])
+}
+
+func printMatrix(m *mat64.Dense) {
+	r, c := m.Dims()
+
+	for i := 0; i < r; i++ {
+		for j := 0; j < c; j++ {
+			fmt.Print(m.At(i, j), " ")
+		}
+		fmt.Println("")
+	}
+}
